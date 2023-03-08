318dbd72857d582d4b139b331ef57028974f7574
lauralvd01 <laura.levraud1@etu.univ-lorraine.fr>
2023-03-08 10:29:08 +0100

    Sign in and sign up working !! Create new user à revoir

--- projetweb/src/routes/signUp/+page.svelte
+++ projetweb/src/routes/signUp/+page.svelte
@@ -4,13 +4,21 @@
     let loading = false
     let email: string
     let password: string
+    let first_name: string
+    let last_name: string
   
-    const handleLogin = async () => {
+    const handleSignUp = async () => {
       try {
         loading = true
-        const { data, error } = await supabase.auth.signInWithPassword({
+        const { data, error } = await supabase.auth.signUp({
           email,
           password,
+          options: {
+            data: {
+              first_name,
+              last_name,
+            },
+          },
          })
         if (error) throw error
       } catch (error) {
@@ -23,10 +31,14 @@
     }
   </script>
   
-  <form class="row flex-center flex" on:submit|preventDefault="{handleLogin}">
+  <form class="row flex-center flex" on:submit|preventDefault="{handleSignUp}">
     <div class="col-6 form-widget">
-      <h1 class="header">Authentification - Sign in</h1>
-      <p class="description">Sign in with your email and password</p>
+      <h1 class="header">Authentification - Sign up</h1>
+      <p class="description">Give us your name and sign up with your email and password</p>
+      <div>
+        <input class="inputField" type="text" placeholder="Your first name" bind:value="{first_name}" />
+        <input class="inputField" type="text" placeholder="Your last name" bind:value="{last_name}" />
+      </div>
       <div>
         <input class="inputField" type="email" placeholder="Your email" bind:value="{email}" />
         <input class="inputField" type="password" placeholder="Your password" bind:value="{password}" />
@@ -34,9 +46,6 @@
       <div>
         <input type="submit" class="button block" value={loading ? 'Loading' : 'Sign in'}
         disabled={loading} />
-      </div>
-      <div>
-        <a href="/sign-up">Sign up</a>
       </div>
     </div>
   </form>
