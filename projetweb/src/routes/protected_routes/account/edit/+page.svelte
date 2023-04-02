df1be9b7a83cfa94f0292ce27b0b0e09a6e07270
Titor07 <dumange.victor@gmail.com>
2023-04-02 17:12:11 +0200

    publish a message

--- projetweb/src/routes/protected_routes/account/edit/+page.svelte
+++ projetweb/src/routes/protected_routes/account/edit/+page.svelte
@@ -7,21 +7,24 @@
     let loading = false;
     onMount(() => {
       getAssos();
+      console.log(myAssociations)
     })
 
     let myTitle : string | null = null;
     let myMessage : string | null = null;
     let myAssociation : string | null = null;
-    let myAssociations = [];
+    let myAssociations: any[] = [];
     let user = $page.data.session.user;
+
 
 
     const addMessage = async () => {
         try {
           loading = true;
+          console.log(myAssociation.association.name_association);
             const {data, error} = await supabase
               .from('post')
-              .insert({association : myAssociation,
+              .insert({association : myAssociation.association.name_association,
                  title: myTitle, 
                  content: myMessage, 
                  id_author: user.id})  
@@ -47,7 +50,8 @@
           .eq('id_user', user.id)
         
         if (data) {
-            myAssociations.push(data);
+            myAssociations = data;
+            console.log(myAssociations);
         }
         if (error) throw error
       } 
@@ -66,7 +70,12 @@
 
     <input bind:value="{myTitle}" type="text" placeholder="+ To Add a title">
     <input bind:value="{myMessage}" type="text" aria-label="Post a message" placeholder=" + to edit a message">
+    <select bind:value={myAssociation}>
+      {#each myAssociations as asso}
+			  <option value={asso}>{asso.association.name_association}</option>
+      {/each}
+	  </select>
     <input type="submit" class="button block primary" value={loading ? 'Loading' : 'POST'}
     disabled={loading} />
-
+    
 </form>
