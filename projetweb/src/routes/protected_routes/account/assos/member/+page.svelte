c4413fdfa6e270b2f4956f172648f03510049f8b
ath-gsy <107257470+ath-gsy@users.noreply.github.com>
2023-04-03 14:05:42 +0200

    finished assos et devenir membre

--- projetweb/src/routes/protected_routes/account/assos/member/+page.svelte
+++ projetweb/src/routes/protected_routes/account/assos/member/+page.svelte
@@ -1,70 +1,66 @@
 <script lang="ts">
 	import { disableScrollHandling, goto } from '$app/navigation';
-import { page } from '$app/stores';
+	import { page } from '$app/stores';
 	import { supabase } from '$lib/supabaseClient';
 	import { onMount } from 'svelte';
 
 	let user = $page.data.session.user;
 
-	let list_asso : any[] = [];
-    let selected_asso : string | null = null;
+	let list_asso: any[] = [];
+	let selected_asso: string | null = null;
 
 	let loading = false;
 
-    onMount(async () => {
-        await getAsso();
-    });
+	onMount(async () => {
+		await getAsso();
+	});
 
-    // Get all associations
-    const getAsso = async () => {
-        try {
-            loading = true;
-            const {data, error} = await supabase
-                .from('association')
-                .select('id,name_association')
-            
-            if (error) throw error;
+	// Get all associations
+	const getAsso = async () => {
+		try {
+			loading = true;
+			const { data, error } = await supabase.from('association').select('id,name_association');
 
-            if (data.length > 0) {
-                list_asso = data;
-                console.log(list_asso);
-            }
-        } catch (error) {
-            if (error instanceof Error) {
-                alert(error.message);
-            }
-        } finally {
-            loading = false;
-        }
-    }
+			if (error) throw error;
 
+			if (data.length > 0) {
+				list_asso = data;
+				console.log(list_asso);
+			}
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			loading = false;
+		}
+	};
 
-    const handleSubmit = async () => {
-        console.log("handleSubmit");
-        if (selected_asso) {
-				try {
-                    loading = true;
-					const { data, error } = await supabase
-						.from('members')
-						.insert({ id_user: user.id, id_asso: selected_asso });
-                    
-                    if (error) throw error;
+	const handleSubmit = async () => {
+		console.log('handleSubmit');
+		if (selected_asso) {
+			try {
+				loading = true;
+				const { data, error } = await supabase
+					.from('members')
+					.insert({ id_user: user.id, id_asso: selected_asso });
 
-                    if (data) {
-                        alert('Vous êtes maintenant membre de cette association');
-                    }
-                    loading = false;
-                    goto('/protected_routes/account/assos');
-				} catch (error) {
-					if (error instanceof Error) {
-						alert(error.message);
-					}
+				if (error) throw error;
+
+				if (data) {
+					alert('Vous êtes maintenant membre de cette association');
+				}
+				loading = false;
+				goto('/protected_routes/account/assos');
+			} catch (error) {
+				if (error instanceof Error) {
+					alert(error.message);
 				}
 			}
-        else {
-            alert('Veuillez choisir une association');
-        }
-    }
+		} else {
+			alert('Veuillez choisir une association');
+		}
+	};
 </script>
 
 <div id="back-button">
@@ -80,38 +76,53 @@ import { page } from '$app/stores';
 </div>
 <div class="myBody">
 	<div class="myBody-text">
-        <p>Choisis ton asso : 
-        <select bind:value={selected_asso}>
-            {#each list_asso as asso}
-                <option value={asso.id} on:change="{() => console.log(selected_asso,asso.name_association)}">
-                    {asso.name_association}
-                </option>
-            {/each}
-        </select>
-        </p>
-        <form id="becomeMemberForm" on:submit|preventDefault={handleSubmit}>
-        <div>
-            <input
-                type="submit"
-                class="button block primary"
-                value={loading ? 'Loading' : 'Devenir membre'}
-                disabled= {loading}
-            />
-        </div>
-        </form>
+		<p>
+			Choisis ton asso :
+			<select id="scrolldown" bind:value={selected_asso}>
+				{#each list_asso as asso}
+					<option
+						value={asso.id}
+						on:change={() => console.log(selected_asso, asso.name_association)}
+					>
+						{asso.name_association}
+					</option>
+				{/each}
+			</select>
+		</p>
+		<form id="formPublish" on:submit|preventDefault={handleSubmit}>
+			<div>
+				<input
+					type="submit"
+					class="button block buttonPerso1"
+					value={loading ? 'Loading' : 'Devenir membre'}
+					disabled={loading}
+				/>
+			</div>
+			<div>
+				<a class="buttonLink" href="/protected_routes/account/assos/create">
+					<button class="buttonPerso2 button block">Créer une association</button>
+				</a>
+			</div>
+		</form>
 	</div>
-    
-    <div>
-        <a class="buttonLink" href="/protected_routes/account/assos/create">
-            <button class="buttonPerso2 button block">Créer une association</button>
-        </a>
-    </div>
 </div>
 
-
 <style>
-	#becomeMemberForm {
-		padding: 0 10%;
+	#scrolldown {
+		width: 60%;
+		height: 1.5rem;
+		border: 1px solid var(--custom-color3);
+		border-radius: 5px;
+		background-color: var(--custom-color3);
+		color: var(--custom-color1-1);
+		font-size: 1rem;
+		padding: 0 10px;
+		margin-top: 1rem;
+	}
+	#formPublish {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 	}
 	/* for return button */
 	#back-button {
@@ -140,5 +151,9 @@ import { page } from '$app/stores';
 	#button1:hover {
 		background-color: #2a2a2a;
 		border: 1px solid var(--custom-color1);
+	}
+
+	.buttonLink {
+		text-decoration: none;
 	}
 </style>
--- projetweb/src/routes/protected_routes/association/[name_association]/logo/+page.svelte
+++ projetweb/src/routes/protected_routes/association/[name_association]/logo/+page.svelte
@@ -1,192 +1,203 @@
 <script lang="ts">
-	import { page } from '$app/stores'
-  import { supabase } from '$lib/supabaseClient'
-  import { onMount } from 'svelte'
-	
+	import { page } from '$app/stores';
+	import { supabase } from '$lib/supabaseClient';
+	import { onMount } from 'svelte';
 
-  export let data
+	export let data;
 
-  let association = data.association;
-   
-  
-  
-  let name_association: string | null = null;
-	
-  let files: FileList;
-  let url_photo: string | null = null;
+	let association = data.association;
+
+	let name_association: string | null = null;
+
+	let files: FileList;
+	let url_photo: string | null = null;
 	let logo: string | null = null;
 	let new_logo: string | null = null;
 
 	let loading = false;
-  let uploading = false;
-  let downloading = false;
-  let deleting = false;
+	let uploading = false;
+	let downloading = false;
+	let deleting = false;
 
 	onMount(() => {
-	  getData();
-	})
+		getData();
+	});
 
 	// GET the association's logo from the database
 	const getData = async () => {
-      try {
-        loading = true
+		try {
+			loading = true;
 
-        let { data, error } = await supabase
-        .from('association')
-        .select('name_association, photo_url')
-        .eq('id', association.id)
-        .single()
-        
+			let { data, error } = await supabase
+				.from('association')
+				.select('name_association, photo_url')
+				.eq('id', association.id)
+				.single();
 
-        if (data) {
-            name_association = data.name_association
-            logo = data.photo_url
-        }
+			if (data) {
+				name_association = data.name_association;
+				logo = data.photo_url;
+			}
 
-        if (error) throw error
-      } 
-      catch (error) {
-        if (error instanceof Error) {
-          alert(error.message)
-        }
-      } 
-      finally {
-        loading = false
-        dowloadLogo();
-      }
-    };
+			if (error) throw error;
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			loading = false;
+			dowloadLogo();
+		}
+	};
 
-    // Upload the association's logo from its file
-    const uploadAvatar = async () => {
-      try {
-        uploading = true
-        
-        const avatarFile = files[0]
-        const { data, error } = await supabase
-          .storage
-          .from('logos')
-          .upload(`${association.id}/avatar_${avatarFile.name}`, avatarFile, {
-            cacheControl: '3600',
-            upsert: true
-          })
+	// Upload the association's logo from its file
+	const uploadAvatar = async () => {
+		try {
+			uploading = true;
 
-        if (data) {
-          new_logo = `avatar_${avatarFile.name}`;
-        }
-        updateLogo();
+			const avatarFile = files[0];
+			const { data, error } = await supabase.storage
+				.from('logos')
+				.upload(`${association.id}/avatar_${avatarFile.name}`, avatarFile, {
+					cacheControl: '3600',
+					upsert: true
+				});
 
-        if (error) throw error
-      } 
-      catch (error) {
-        if (error instanceof Error) {
-          alert(error.message)
-        }
-      } 
-      finally {
-        uploading = false
-      }
-    }
+			if (data) {
+				new_logo = `avatar_${avatarFile.name}`;
+			}
+			updateLogo();
 
-    // Update the association's logo in the database
-    const updateLogo = async () => {
-      try {
-        uploading = true
-        let { error } = await supabase
-        .from('association')
-        .update({ photo_url: new_logo? new_logo : logo })
-        .eq('id', association.id)
+			if (error) throw error;
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			uploading = false;
+		}
+	};
 
-        if (error) throw error
-      } 
-      catch (error) {
-        if (error instanceof Error) {
-          alert(error.message);
-        }
-      } 
-      finally {
-        uploading = false
-        getData();
-      }
-    }
+	// Update the association's logo in the database
+	const updateLogo = async () => {
+		try {
+			uploading = true;
+			let { error } = await supabase
+				.from('association')
+				.update({ photo_url: new_logo ? new_logo : logo })
+				.eq('id', association.id);
 
-    // Download the association's logo from the database
-    const dowloadLogo = async () => {
-      if (logo) {
-        try {
-          downloading = true
-          const { data, error } = await supabase
-          .storage
-          .from('logos')
-          .download(`${association.id}/${logo}`)
+			if (error) throw error;
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			uploading = false;
+			getData();
+		}
+	};
 
-          if (data) {
-              url_photo = URL.createObjectURL(data);
-          }
+	// Download the association's logo from the database
+	const dowloadLogo = async () => {
+		if (logo) {
+			try {
+				downloading = true;
+				const { data, error } = await supabase.storage
+					.from('logos')
+					.download(`${association.id}/${logo}`);
 
-          if (error) throw error
-        } 
-        catch (error) {
-          if (error instanceof Error) {
-            alert(error.message)
-          }
-        } 
-        finally {
-          downloading = false;
-        }
-      }
-    }
+				if (data) {
+					url_photo = URL.createObjectURL(data);
+				}
 
-    // Delete the user's photo (only in the user's table)
-    const deleteLogo = async () => {
-      try {
-        deleting = true
-        let { error } = await supabase
-        .from('association')
-        .update({ photo_url: null })
-        .eq('id', association.id)
+				if (error) throw error;
+			} catch (error) {
+				if (error instanceof Error) {
+					alert(error.message);
+				}
+			} finally {
+				downloading = false;
+			}
+		}
+	};
 
-        logo = null;
-        new_logo = null;
-        url_photo = null;
+	// Delete the user's photo (only in the user's table)
+	const deleteLogo = async () => {
+		try {
+			deleting = true;
+			let { error } = await supabase
+				.from('association')
+				.update({ photo_url: null })
+				.eq('id', association.id);
 
-        if (error) throw error
-      } 
-      catch (error) {
-        if (error instanceof Error) {
-          alert(error.message);
-        }
-      } 
-      finally {
-        deleting = false;
-        getData();
-      }
-    }
+			logo = null;
+			new_logo = null;
+			url_photo = null;
+
+			if (error) throw error;
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			deleting = false;
+			getData();
+		}
+	};
 </script>
 
-{#if url_photo}
-<div class="avatar image flex-center" >
-  <img src={url_photo} alt={downloading ? 'Downloading ...' : ''} class="avatar image flex-center" style="padding-left: 30%;"/>
-</div>
-{/if}
-
-<div class="body">
-    <label class="button primary block" for="single">
-      {uploading ? 'Uploading ...' : 'Upload a new photo'}
-    </label>
-    <span style="display:none">
-      <input
-        type="file"
-        id="single"
-        accept="image/*"
-        bind:files
-        on:change="{uploadAvatar}"
-        disabled="{uploading}"
-      />
-    </span>
+<div id="back-button">
+	<a id="return" href="/protected_routes/association">
+		<button id="button1" class="button buttonPerso3 block primary"> &lt Retour</button>
+	</a>
 </div>
 
-<div class="body">
-  <button class="button primary block" on:click={deleteLogo}>{deleting ? 'Deleting ...' : 'Delete the photo'}</button>
+<div class="myTitle" style="background-color: var(--custom-color3)">
+	<div class="myTitle-text">
+		<h1 class="header">Changer la photo de l'asso</h1>
+	</div>
+</div>
+<div class="myBody">
+	<div class="myBody-text">
+		<div class="row">
+			<div class="column">
+				{#if url_photo}
+					<div class="avatar image flex-center">
+						<img
+							src={url_photo}
+							alt={downloading ? 'Downloading ...' : ''}
+							class="avatar image flex-center"
+							style="padding-left: 30%;"
+						/>
+					</div>
+				{/if}
+			</div>
+			<div class="column">
+				<div class="body">
+					<label class="button buttonPerso2 block" for="single">
+						{uploading ? 'Uploading ...' : 'Télécharger une nouvelle photo'}
+					</label>
+					<span style="display:none">
+						<input
+							type="file"
+							id="single"
+							accept="image/*"
+							bind:files
+							on:change={uploadAvatar}
+							disabled={uploading}
+						/>
+					</span>
+				</div>
+
+				<div class="body">
+					<button class="button buttonPerso4 block" on:click={deleteLogo}
+						>{deleting ? 'Deleting ...' : 'Supprimer la photo'}</button
+					>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <style>
